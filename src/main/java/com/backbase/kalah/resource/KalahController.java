62d76d8feadd5110cb69527af2e941e4438f3eef
diegotoro <diegotoro@192.168.100.28>
2018-03-15 20:00:03 -0500

    Initial setup

---
+++ src/main/java/com/backbase/kalah/resource/KalahController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.backbase.kalah.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author diegotoro
+ *
+ */
+@RestController(value = "/kalah")
+public class KalahController {
+
+
+}
