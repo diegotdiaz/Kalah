7b7bb53f26a356db2c479e1798d09b04e4ec0144
Diego Toro Diaz <dtoro@eharmony.com>
2018-03-20 21:23:29 -0700

    Adding missing Messages class

---
+++ src/main/java/com/backbase/kalah/util/Messages.java
@@ -0,0 +1,15 @@
+package com.backbase.kalah.util;
+
+/**
+ * Messages class
+ * @author dtoro
+ *
+ */
+public final class Messages {
+
+	public static final String MESSAGE_APP_IS_UP = "Kalah App is Up";
+	public static final String MESSAGE_NOT_FOUND = "Game with ID %s was not found";
+	public static final String MESSAGE_ILLEGAL_MOVE = "Illegal move, %s may only take seeds from pit %d to %d";
+	public static final String MESSAGE_WRONG_TURN = "It' not %s's turn";
+	public static final String MESSAGE_GAME_FINISHED = "Game is finished";
+}
